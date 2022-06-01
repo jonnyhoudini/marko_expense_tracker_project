dc7c90200ec270246bdda7b9605c1e5d30cd8fdb
jonnyhoudini <jmcaveety@gmail.com>
2022-06-01 21:36:59 +0100

    deleted unused change limit method

--- app.py
+++ app.py
@@ -18,3 +18,7 @@ app.register_blueprint(users_blueprint)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/questions')
+def questions():
+    return render_template('questions.html')
--- tests/user_test.py
+++ tests/user_test.py
@@ -10,7 +10,3 @@ class UserTest(unittest.TestCase):
     def test_user_has_name(self):
         self.assertEqual("Theodore Templeton", self.user.name)
 
-    def test_user_limit_can_be_changed(self):
-        self.user.change_limit(1500)
-        self.assertEqual(1500, self.user.limit)
-
